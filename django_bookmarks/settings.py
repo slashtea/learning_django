7db15df6ee06676a17e5a86a28f0110fd0e5b39e
Ziyad Mestour <ziyad.mestour@gmail.com>
2015-09-16 15:37:31 +0100

    Fixing the bootstrap3

--- django_bookmarks/settings.py
+++ django_bookmarks/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap',
+    'bootstrap3',
 )
 
 MIDDLEWARE_CLASSES = (
